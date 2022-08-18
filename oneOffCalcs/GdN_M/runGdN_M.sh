e68c4197002bf6c2925bb01c395a8757a3c6fe97
Ludoric <ludoric3435@gmail.com>
2022-08-18 16:22:24 +1200

    Maybe GdN_M will actually work this time?

--- oneOffCalcs/GdN_M/runGdN_M.sh
+++ oneOffCalcs/GdN_M/runGdN_M.sh
@@ -27,6 +27,8 @@ module load intel/2021b
 # !!! use the lattice output from vc-relax as input to scf and wannier90
 # mpirun -np 64 "$BINLOC/pw.x" -npool 4 -in GdN_M_scf.pw.in | tee GdN_M_scf.pw.out
 mpirun -np 64 "$BINLOC/pw.x" -npool 4 -in GdN_M_nscf.pw.in | tee GdN_M_nscf.pw.out
+
+# !!! run this section on a single CPU - it'll take a while, but at least will finish
 # Run wannier90 to generate a list of the required overlaps (written into the Fe.nnkp file).
 # !!!! mpi not available for wannier90.x
 $BINLOC/wannier90.x -pp GdN_M_up | tee GdN_M_dw_1.wannier90.out
@@ -37,12 +39,18 @@ $BINLOC/wannier90.x -pp GdN_M_dw | tee GdN_M_dw_1.wannier90.out
 # – The matrix elements <h_{unk+b1}|H_k|u_{mk+b2i}> (written in the Fe.uHu file)
 # !!!! pools not implemented for pw2wannier90.x
 # !!!! there may not be more processors than bands created in GdN_M.win
-mpirun -np 25 "$BINLOC/pw2wannier90.x" -in GdN_M_up.pw2wan.in | tee GdN_M_up.pw2wan.out
-mpirun -np 25 "$BINLOC/pw2wannier90.x" -in GdN_M_dw.pw2wan.in | tee GdN_M_dw.pw2wan.out
+# !!!! mpi version doesn't support output to .uHu file
+# mpirun -np 25 "$BINLOC/pw2wannier90.x" -in GdN_M_up.pw2wan.in | tee GdN_M_up.pw2wan.out
+# mpirun -np 25 "$BINLOC/pw2wannier90.x" -in GdN_M_dw.pw2wan.in | tee GdN_M_dw.pw2wan.out
+$BINLOC/pw2wannier90.x < GdN_M_up.pw2wan.in | tee GdN_M_up.pw2wan.out
+$BINLOC/pw2wannier90.x < GdN_M_dw.pw2wan.in | tee GdN_M_dw.pw2wan.out
 # Run wannier90 to compute the MLWFs.
 # !!!! mpi not available for wannier90.x
 $BINLOC/wannier90.x GdN_M_up | tee GdN_M_up_2.wannier90.out
 $BINLOC/wannier90.x GdN_M_dw | tee GdN_M_dw_2.wannier90.out
+
+
+# !!! run this one on multiple cores again
 # Run postw90 to compute the orbital magnetization.
 mpirun -np 64 $BINLOC/postw90.x GdN_M_up | tee GdN_M_up_1.postw90.out
 mpirun -np 64 $BINLOC/postw90.x GdN_M_dw | tee GdN_M_dw_1.postw90.out
